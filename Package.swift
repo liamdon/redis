40ca73e38b9a1402d9b296a2ce120a1485a08564
Liam Don <liamdon@gmail.com>
2020-01-07 16:59:11 -0800

    Use redis kit fork

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
         .library(name: "Redis", targets: ["Redis"])
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/redis-kit.git", from: "1.0.0-beta.2"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta.2.1"),
+        .package(url: "https://github.com/liamdon/redis-kit.git", from: "1.0.0-beta.3-lua"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta.3"),
     ],
     targets: [
       .target(name: "Redis", dependencies: ["RedisKit", "Vapor"]),
